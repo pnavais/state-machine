31ab17107b1b2c322aa84115b1a72da26fb370cd
Pablo Navais <pnavais@gmail.com>
2019-08-13 09:17:35 +0200

    Added missing hashCode

--- src/main/java/com/github/pnavais/machine/model/SimpleEnvelope.java
+++ src/main/java/com/github/pnavais/machine/model/SimpleEnvelope.java
@@ -42,4 +42,7 @@ public class SimpleEnvelope extends AbstractEnvelope<State, Message> {
         return super.equals(o);
     }
 
+    @Override
+    public int hashCode() { return super.hashCode(); }
+
 }
